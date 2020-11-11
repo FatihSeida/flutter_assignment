9016b7e2a18ddb6d0c1ed31d461e431cc6c27833
Fatih Seida <fatihseidaaaaa@gmail.com>
2020-11-11 08:21:21 +0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,36 @@
-// 1) Create a new Flutter App (in this project) and output an AppBar and some text
-// below it
-// 2) Add a button which changes the text (to any other text of your choice)
-// 3) Split the app into three widgets: App, TextControl & Text
-
 import 'package:flutter/material.dart';
+import './substring_highlight.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Assignment',
-      home: MyCustomForm(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyCustomForm(title: 'Flutter Assignment'),
     );
   }
 }
 
+class MatchPattern {
+  final int startIndex;
+  final int length;
+  const MatchPattern(this.startIndex, this.length);
+}
+
 class MyCustomForm extends StatefulWidget {
+  MyCustomForm({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _MyCustomFormState createState() => _MyCustomFormState();
 }
@@ -26,13 +39,28 @@ class _MyCustomFormState extends State<MyCustomForm> {
   final pertanyaanController = TextEditingController();
   final jawabanController = TextEditingController();
   final kataKunciController = TextEditingController();
-  String kataKunci = "";
-  String jawaban = "";
+  final pertanyaanFocus = FocusNode();
+  final jawabanFocus = FocusNode();
+  final kataKunciFocus = FocusNode(); //length na misal 3
+  var kKsplit = [];
 
-  static int noOfCharacter = 256;
+  List<MatchPattern> matchPattern = [];
+  List<String> multiplePattern = [];
+  var split;
+  //indeks hasil pencarian, misal 101, loop ker di tampil = 101, 102, 103
+  bool isLoading = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    pertanyaanController.dispose();
+    jawabanController.dispose();
+  }
+
   int maxOfAnB(int a, int b) => (a > b) ? a : b;
 
   List<int> badCharacterHeuristic(String str) {
+    int noOfCharacter = 20000;
     List<int> badChar = new List<int>(noOfCharacter);
     int i;
 
@@ -40,144 +68,190 @@ class _MyCustomFormState extends State<MyCustomForm> {
       badChar[i] = -1;
     }
 
-    for (i = 0; i < str.length - 1; i++) {
-      var index = str.indexOf(str[i]);
-      badChar[index] = i;
+    for (i = 0; i < str.length; i++) {
+      badChar[str.codeUnitAt(i)] = i;
     }
+
     return badChar;
   }
 
-  // A kataKunci searching function
-  void search(String jawaban, String kataKunci) {
-    int m = kataKunci.length - 1;
-    int n = jawaban.length - 1;
-
-    List<int> badchar = badCharacterHeuristic(kataKunci);
+  Future<void> search(String text, String pattern) async {
+    int m = pattern.length;
+    int n = text.length;
+    print("Len $pattern = $m");
+    List<int> badchar = badCharacterHeuristic(pattern);
 
     int s = 0;
     while (s <= (n - m)) {
       int j = m - 1;
-      while (j >= 0 && kataKunci[j] == jawaban[s + j]) {
-        Text(kataKunci[j]);
-        Text(jawaban[s + j]);
-        j--;
-      }
+      while (j >= 0 && pattern[j] == text[s + j]) j--;
       if (j < 0) {
-        print("kataKunci occur at shift = $s");
-        var index = jawaban.indexOf(jawaban[s + m]);
-        s += (s + m < n) ? m - badchar[index] : 1;
+        print("Patterns occur at shift = $s");
+        //TODO: refactor
+        matchPattern.add(MatchPattern(s, m));
+        s += (s + m < n) ? m - badchar[text.codeUnitAt(s + m)] : 1;
       } else {
-        var index = jawaban.indexOf(jawaban[s + j]);
-        s += maxOfAnB(1, j - badchar[index]);
+        s += maxOfAnB(1, j - badchar[text.codeUnitAt(s + j)]);
       }
     }
   }
 
-  @override
-  void dispose() {
-    pertanyaanController.dispose();
-    jawabanController.dispose();
-    kataKunciController.dispose();
-    super.dispose();
+  void submit() {
+    setState(() {
+      isLoading = true;
+      matchPattern.clear();
+    });
+    var split = kataKunciController.text.split(', ');
+    if (split.length > 1) {
+      Future.forEach(split, (element) async {
+        return await search(jawabanController.text, element);
+      }).then((value) => Future.delayed(Duration(seconds: 1))).whenComplete(() {
+        setState(() {
+          isLoading = false;
+        });
+      });
+    } else {
+      search(jawabanController.text, kataKunciController.text)
+          .then((value) async {
+        return await Future.delayed(Duration(seconds: 1));
+      }).whenComplete(() {
+        setState(() {
+          isLoading = false;
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final cursorColor = Theme.of(context).cursorColor;
-    const sizedBoxSpace = SizedBox(height: 24);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Assignment'),
-      ),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            sizedBoxSpace,
-            TextFormField(
-              controller: pertanyaanController,
-              cursorColor: cursorColor,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: 'Pertanyaan',
-                labelText: 'Pertanyaan',
-              ),
-            ),
-            sizedBoxSpace,
-            TextFormField(
-              controller: jawabanController,
-              cursorColor: cursorColor,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: 'Jawaban',
-                labelText: 'Jawaban',
-              ),
-            ),
-            sizedBoxSpace,
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                    width: 2,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: pertanyaanController,
+                  focusNode: pertanyaanFocus,
+                  onSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(jawabanFocus);
+                  },
+                  decoration: const InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Pertanyaan',
                   ),
-                  borderRadius: BorderRadius.circular(4)),
-              padding: EdgeInsets.only(left: 20.0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    jawaban,
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  controller: jawabanController,
+                  focusNode: jawabanFocus,
+                  onSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(kataKunciFocus);
+                  },
+                  decoration: const InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Jawaban',
                   ),
-                ],
-              ),
-            ),
-            sizedBoxSpace,
-            TextFormField(
-              controller: kataKunciController,
-              cursorColor: cursorColor,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: 'Kata Kunci',
-                labelText: 'Kata Kunci',
-              ),
-            ),
-            sizedBoxSpace,
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                    width: 2,
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  controller: kataKunciController,
+                  focusNode: kataKunciFocus,
+                  decoration: const InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Kata Kunci',
                   ),
-                  borderRadius: BorderRadius.circular(4)),
-              padding: EdgeInsets.only(left: 20.0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    kataKunci,
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
+                ),
+                SizedBox(height: 16),
+                ButtonTheme(
+                  minWidth: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  textTheme: ButtonTextTheme.primary,
                   child: RaisedButton(
-                    child: Text('Submit'),
-                    onPressed: () {
-                      setState(() {
-                        jawaban = jawabanController.text;
-                        kataKunci = kataKunciController.text;
-                        search(jawaban, kataKunci);
-                      });
-                    },
+                    onPressed: () => submit(),
+                    child: Text("Submit"),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Flexible(
+                  child: Card(
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(8.0),
+                      child: isLoading
+                          ? Center(child: CircularProgressIndicator())
+                          : Center(
+                              child: Directionality(
+                                textDirection: TextDirection.ltr,
+                                child: res(),
+                              ),
+                            ),
+                    ),
                   ),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget res() {
+    if (matchPattern.length > 0) {
+      return Column(
+        children: [
+          Text("${matchPattern.length} / ${split.length} match pattern"),
+          Divider(),
+          Text.rich(
+            TextSpan(
+              children: getResult().toList(),
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      );
+    } else {
+      return Text("Tidak ada kata kunci yang cocok");
+    }
+  }
+
+  List<TextSpan> getResult() {
+    List<TextSpan> res = [];
+    List<String> kata = jawabanController.text.toLowerCase().split('');
+    split = kataKunciController.text.toLowerCase().split(', ');
+    if (split.length > 1) {}
+    // int patlen = kataKunciController.text.length;
+
+    for (int i = 0; i < kata.length; i++) {
+      if (matchPattern.where((e) => e.startIndex == i).isNotEmpty) {
+        var found = matchPattern.singleWhere((e) => e.startIndex == i);
+        res.add(
+          TextSpan(
+            text: jawabanController.text
+                .substring(found.startIndex, found.startIndex + found.length),
+            style: TextStyle(
+              color: Colors.yellow,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        );
+        i += found.length;
+        // kata.removeRange(i, i + patlen);
+      }
+      if (i < kata.length) {
+        res.add(
+          TextSpan(text: kata[i]),
+        );
+        // i += patlen;
+      }
+    }
+
+    return res;
   }
 }
